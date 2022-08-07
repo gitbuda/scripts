936f90f8c47ce12f4563731b50fd8f5aaf158296
Marko Budiselic <mbudiselicbuda@gmail.com>
2022-08-07 19:01:46 +0200

    Fix treesitter config

--- nvchad/chadrc.lua
+++ nvchad/chadrc.lua
@@ -1,7 +1,25 @@
 local M = {}
 
 M.plugins = {
-        user = require "custom.plugins"
+   override = {
+      ["nvim-treesitter/nvim-treesitter"] = {
+         ensure_installed = {
+            "vim",
+            "html",
+            "css",
+            "javascript",
+            "json",
+            "toml",
+            "markdown",
+            "c",
+            "cpp",
+            "bash",
+            "lua",
+            "norg",
+         }
+      },
+   },
+   user = require "custom.plugins"
 }
 
 M.mappings = require "custom.mappings"
