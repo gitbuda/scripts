55ab8cb875d7878e6f128882ffc2f558b0626bdd
Marko Budiselic <mbudiselicbuda@gmail.com>
2022-10-15 06:32:27 -0400

    Add nvim, nvchad, bashrc

--- env-available/server-fedora-36/setup.sh
+++ env-available/server-fedora-36/setup.sh
@@ -3,6 +3,8 @@
 DEPS=(
     htop git vim tmux
     sysbench stress-ng
+    cmake make gcc clang
+    libtool custom-neovim custom-nvchad
 )
 
 script_dir="$( cd "$(dirname "$([ -L "$0" ] && readlink -f "$0" || echo "$0")")" && pwd)"
@@ -16,15 +18,45 @@ if [ "$SUDO_USER" == "" ]; then
     echo "Please run as sudo."
 fi
 
-PUBLIC_DOTFILES="gitconfig gitignore"
+PUBLIC_DOTFILES="gitconfig gitignore bash_aliases"
 for f in ${PUBLIC_DOTFILES}; do
     rm -rf "/home/$SUDO_USER/.$f"
     ln -s "${script_dir}/../../dotfiles/$f" "/home/$SUDO_USER/.$f"
 done
 
+LOCAL_DOTFILES="bashrc"
+for f in ${LOCAL_DOTFILES}; do
+    rm -rf "/home/$SUDO_USER/.$f"
+    ln -s "${script_dir}/$f" "/home/$SUDO_USER/.$f"
+done
+
 for pkg in "${DEPS[@]}"; do
     # if [ "$pkg" ==  ]; then
     # fi
+
+    if [ "$pkg" == custom-neovim ]; then
+        if ! bin_installed "$script_dir/neovim/build/bin/nvim"; then
+            cd "$script_dir"
+            git clone https://github.com/neovim/neovim
+            chown -R "$SUDO_USER:$SUDO_USER" "$script_dir/neovim"
+            cd neovim
+            runuser -u "$SUDO_USER" -p -- git checkout v0.7.2
+            runuser -u "$SUDO_USER" -p -- make CMAKE_BUILD_TYPE=Release -j8
+            make install
+        fi
+        echo "$pkg is installed." && continue
+    fi
+
+    if [ "$pkg" == custom-nvchad ]; then
+        if [ ! -d "/home/$SUDO_USER/.config/nvim" ]; then
+            git clone https://github.com/NvChad/NvChad "/home/$SUDO_USER/.config/nvim"
+            chown -R "$SUDO_USER:$SUDO_USER" "/home/$SUDO_USER/.config/nvim"
+        fi
+        if [ ! -L "/home/$SUDO_USER/.config/nvim/lua/custom" ]; then
+          ln -s "${script_dir}/../../nvchad" "/home/$SUDO_USER/.config/nvim/lua/custom"
+        fi
+        echo "$pkg is installed." && continue
+    fi
 
     if ! dnf_installed "$pkg"; then
         dnf install -y "$pkg"
