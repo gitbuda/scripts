8b43b6a3cffbe1c9529f7c7ddddeaa2a6ba26aea
Marko Budiselic <mbudiselicbuda@gmail.com>
2022-11-01 23:11:52 +0100

    Add gnuplot, pandoc, fonts

--- env-available/macos-m1-12/setup.sh
+++ env-available/macos-m1-12/setup.sh
@@ -1,17 +1,32 @@
 #!/bin/bash -e
 
+# TODO: xelatex https://gist.github.com/peterhurford/75957ba9335e755013b87254ec85fab1
+
 DEPS=(
     git htop tmux nvim
     custom-xcode
     ffmpeg
+    gnuplot
+    pandoc basictex
     qcachegrind
     virtualenv
     custom-nvchad
+    custom-fonts
 )
 
 script_dir="$( cd "$(dirname "$([ -L "$0" ] && readlink -f "$0" || echo "$0")")" && pwd)"
 # shellcheck disable=SC1090
 source "$script_dir/../../util/os_util"
+
+function install_font {
+    download_link=$1
+    local_file_name=$2
+    if [ ! -f "$local_file_name" ]; then
+        wget "$download_link" -O "$local_file_name"
+        mkdir -p "/Users/$USER/Library/Fonts"
+        unzip "$local_file_name" -d "/Users/$USER/Library/Fonts"
+    fi
+}
 
 PUBLIC_DOTFILES="gitconfig gitignore"
 for f in ${PUBLIC_DOTFILES}; do
@@ -34,6 +49,12 @@ for pkg in "${DEPS[@]}"; do
         if [ ! -d "$NVCHAD_DIR" ]; then
             git clone https://github.com/NvChad/NvChad $NVCHAD_DIR --depth 1
         fi
+        echo "$pkg is installed." && continue
+    fi
+
+    if [ "$pkg" == custom-fonts ]; then
+        cd "$script_dir"
+        install_font "https://dl.dafont.com/dl/?f=roboto" "Roboto.zip"
         echo "$pkg is installed." && continue
     fi
 
