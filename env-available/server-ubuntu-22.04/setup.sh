135eb33eaf0be712d478b172af4046341f55e731
Marko Budiselić <mbudiselicbuda@gmail.com>
2024-04-04 21:57:03 +0200

    Update setup of Ubuntu 22.04 Server (#16)

--- env-available/server-ubuntu-22.04/setup.sh
+++ env-available/server-ubuntu-22.04/setup.sh
@@ -1,26 +1,40 @@
 #!/bin/bash -e
+script_dir="$( cd "$(dirname "$([ -L "$0" ] && readlink -f "$0" || echo "$0")")" && pwd)"
+# shellcheck disable=SC1090
+source "$script_dir/../../util/os_util"
 
 RM_DEPS=(
-#   rm_neovim rm_nvchad
+    # rm_neovim
+    # rm_nvchad
+    # rm_fzf
+    # rm_custom-conda
 )
 DEPS=(
     htop tmux vim tree curl git tig dialog silversearcher-ag zsh plocate
-    custom-fzf
-    make cmake libssl-dev pkg-config libtool-bin unzip gettext
-    python3-dbg
+    make cmake cmake-curses-gui libssl-dev pkg-config libtool-bin unzip gettext
+    ripgrep
+    exuberant-ctags
+    python3-dbg python3.10-venv
     openjdk-17-jre
     ansible
     memtester
     heaptrack
+    sysstat iotop nvtop
     custom-neovim custom-nvchad
+    custom-fzf
     custom-nvm
     custom-rust
+    nvidia-cuda-toolkit
+    # custom-cudnn custom-nccl custom-cutensor custom-cusparselt
+    # custom-just # https://just.systems/man/en/chapter_4.html -> cargo install just
+    # custom-mevi # https://github.com/fasterthanlime/mevi
+      # sudo sysctl -w vm.unprivileged_userfaultfd=1
+      # cargo install just trunk
+    cargo-tree-sitter
+    custom-conda
+    nvidia-cuda-toolkit # custom-cuda
 )
 # TODO(gitbuda): Add e.g. https://github.com/leehblue/texpander
-
-script_dir="$( cd "$(dirname "$([ -L "$0" ] && readlink -f "$0" || echo "$0")")" && pwd)"
-# shellcheck disable=SC1090
-source "$script_dir/../../util/os_util"
 
 function rm_neovim {
     echo "Removing neovim"
@@ -34,15 +48,14 @@ function rm_nvchad {
     rm -rf $1/.cache/nvim
 }
 
-function install_font {
-    download_link=$1
-    local_file_name=$2
-    if [ ! -f "$local_file_name" ]; then
-        wget "$download_link" -O "$local_file_name"
-        mkdir -p "/home/$SUDO_USER/.fonts"
-        unzip "$local_file_name" -d "/home/$SUDO_USER/.fonts"
-        fc-cache -fv
-    fi
+function rm_fzf {
+  echo "Removing fzf"
+  rm -rf $1/.fzf
+}
+
+function rm_custom-conda {
+  echo "Removing custom-conda"
+  rm -rf $1/miniconda3
 }
 
 if [ "$EUID" -ne 0 ]; then
@@ -53,8 +66,14 @@ if [ "$SUDO_USER" == "" ]; then
 fi
 HOME=/home/$SUDO_USER
 
-DOTFILES=""
-for f in ${DOTFILES}; do
+PUBLIC_DOTFILES="bash_aliases"
+for f in ${PUBLIC_DOTFILES}; do
+    rm -rf "/home/$SUDO_USER/.$f"
+    sudo -H -u "$SUDO_USER" bash -c "ln -s ${script_dir}/../../dotfiles/$f /home/$SUDO_USER/.$f"
+done
+
+LOCAL_DOTFILES="bashrc tmux.conf"
+for f in ${LOCAL_DOTFILES}; do
     rm -rf "/home/$SUDO_USER/.$f"
     sudo -H -u "$SUDO_USER" bash -c "ln -s ${script_dir}/$f /home/$SUDO_USER/.$f"
 done
@@ -63,14 +82,74 @@ for rm_pkg in "${RM_DEPS[@]}"; do
     "$rm_pkg" "$HOME"
 done
 
+cd "$script_dir"
 for pkg in "${DEPS[@]}"; do
     # if [ "$pkg" ==  ]; then
     # fi
+
+    if [ "$pkg" == custom-cudnn ]; then
+        installed_install_path="/var/cudnn-local-repo-ubuntu2204-8.8.0.121"
+        cudnn_deb_packet="cudnn-local-repo-ubuntu2204-8.8.0.121_1.0-1_amd64.deb"
+        cudnn_gpg_path="$installed_install_path/cudnn-local-04B81517-keyring.gpg"
+        if [ ! -f "$cudnn_gpg_path" ]; then
+          curl -O -J -L "https://developer.download.nvidia.com/compute/redist/cudnn/v8.8.0/local_installers/12.0/$cudnn_deb_packet"
+          dpkg -i "$cudnn_deb_packet"
+          cp $cudnn_gpg_path /usr/share/keyrings/
+        fi
+        if ! deb_installed "libcudnn8"; then
+          dpkg -i "$installed_install_path/libcudnn8_8.8.0.121-1+cuda12.0_amd64.deb"
+          echo "libcudnn installed"
+        fi
+        if ! deb_installed "libcudnn8-dev"; then
+          dpkg -i "$installed_install_path/libcudnn8-dev_8.8.0.121-1+cuda12.0_amd64.deb"
+          echo "libcudnn-dev installed"
+        fi
+        echo "$pkg is installed." && continue
+    fi
+
+    if [ "$pkg" == custom-nccl ]; then
+        if [ ! -f "/usr/local/include/nccl.h" ]; then
+          rm -rf nccl
+          git clone https://github.com/NVIDIA/nccl.git
+          cd "$script_dir/nccl"
+          # NOTE: CUDA_HOME depends on how cuda is actually installed.
+          # NOTE: g++-10 is here because of https://github.com/NVIDIA/nccl/issues/102
+          CXX=/usr/bin/g++-10 make -j src.build CUDA_HOME=/usr/lib/nvidia-cuda-toolkit
+          make install
+          cd "$script_dir"
+        fi
+        echo "$pkg is installed." && continue
+    fi
+
+    if [ "$pkg" == custom-cutensor ]; then
+        # https://developer.nvidia.com/cutensor-downloads?target_os=Linux&target_arch=x86_64&Distribution=Ubuntu&target_version=22.04&target_type=deb_local
+        if ! deb_installed "libcutensor2"; then
+          curl -O -J -L https://developer.download.nvidia.com/compute/cutensor/2.0.0/local_installers/cutensor-local-repo-ubuntu2204-2.0.0_1.0-1_amd64.deb
+          dpkg -i cutensor-local-repo-ubuntu2204-2.0.0_1.0-1_amd64.deb
+          cp /var/cutensor-local-repo-ubuntu2204-2.0.0/cutensor-*-keyring.gpg /usr/share/keyrings/
+          apt update
+          apt-get -y install libcutensor2 libcutensor-dev libcutensor-doc
+        fi
+        echo "$pkg is installed." && continue
+    fi
+
+    if [ "$pkg" == custom-cusparselt ]; then
+        # https://developer.nvidia.com/cusparselt-downloads?target_os=Linux&target_arch=x86_64&Distribution=Ubuntu&target_version=22.04&target_type=deb_local
+        if ! deb_installed "libcusparselt0"; then
+          curl -O -J -L https://developer.download.nvidia.com/compute/cusparselt/0.5.2/local_installers/cusparselt-local-repo-ubuntu2204-0.5.2_1.0-1_amd64.deb
+          dpkg -i cusparselt-local-repo-ubuntu2204-0.5.2_1.0-1_amd64.deb
+          cp /var/cusparselt-local-repo-ubuntu2204-0.5.2/cusparselt-*-keyring.gpg /usr/share/keyrings/
+          apt-get update
+          apt-get -y install libcusparselt0 libcusparselt-dev
+        fi
+        echo "$pkg is installed." && continue
+    fi
 
     if [ "$pkg" == custom-rust ]; then
         if ! bin_installed "/home/$SUDO_USER/.cargo/bin/rustup"; then
             sudo -H -u "$SUDO_USER" bash -c "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh"
         fi
+        sudo -H -u "$SUDO_USER" bash -c "/home/$SUDO_USER/.cargo/bin/rustup update"
         echo "$pkg is installed." && continue
     fi
 
@@ -79,7 +158,7 @@ for pkg in "${DEPS[@]}"; do
             cd "$script_dir"
             git clone https://github.com/neovim/neovim
             cd neovim
-            git checkout v0.8.0
+            git checkout v0.9.5
             chown -R "$SUDO_USER:$SUDO_USER" "$script_dir/neovim"
             sudo -H -u "$SUDO_USER" bash -c "make CMAKE_BUILD_TYPE=Release -j4"
             make install
@@ -89,12 +168,15 @@ for pkg in "${DEPS[@]}"; do
 
     if [ "$pkg" == custom-nvchad ]; then
         if [ ! -d "/home/$SUDO_USER/.config/nvim" ]; then
-            GIT_SSH_COMMAND="ssh -i /home/$SUDO_USER/.ssh/github" git clone git@github.com:NvChad/NvChad.git "/home/$SUDO_USER/.config/nvim" --depth 1
-            chown -R "$SUDO_USER:$SUDO_USER" "/home/$SUDO_USER/.config/nvim"
+            # sudo -H -u "$SUDO_USER" bash -c "git clone git@github.com:NvChad/starter.git '/home/$SUDO_USER/.config/nvim'"
+            # chown -R "$SUDO_USER:$SUDO_USER" "/home/$SUDO_USER/.config/nvim"
+            # cd "/home/$SUDO_USER/.config/nvim"
+            # git checkout v2.5
+            if [ ! -L "/home/$SUDO_USER/.config/nvim" ]; then
+               ln -s "/home/$SUDO_USER/scripts/nvchad-v2.5" "/home/$SUDO_USER/.config/nvim"
+            fi
         fi
-        if [ ! -L "/home/$SUDO_USER/.config/nvim/lua/custom" ]; then
-            ln -s "/home/$SUDO_USER/scripts/nvchad3" "/home/$SUDO_USER/.config/nvim/lua/custom"
-        fi
+
         echo "$pkg is installed." && continue
     fi
 
@@ -123,11 +205,53 @@ for pkg in "${DEPS[@]}"; do
         echo "$pkg is installed." && continue
     fi
 
+    if [ "$pkg" == custom-conda ]; then
+        # TODO(gitbuda): Decide if it's wise to edit, e.g., bashrc -> probably not.
+        if [ ! -d "/home/$SUDO_USER/miniconda3" ]; then
+            cd "$script_dir"
+            wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+            # NOTE: -b stands for install in batch mode (without manual intervention).
+            bash miniconda.sh -b
+        fi
+        echo "$pkg is installed." && continue
+    fi
+
+    if [ "$pkg" == custom-cuda ]; then
+        # TODO(gitbuda): Revisit how to skip custom installation. NOTE: there is also nvidia-cuda-toolkit, if here, remove the default package.
+        if [ ! -d "/usr/local/cuda" ]; then
+            cd "$script_dir"
+            wget https://developer.download.nvidia.com/compute/cuda/11.6.0/local_installers/cuda_11.6.0_510.39.01_linux.run -O cuda.out
+            sh cuda.out
+        fi
+        echo "$pkg is installed." && continue
+    fi
+
+    if [ "$pkg" == cargo-tree-sitter ]; then
+        sudo -H -u "$SUDO_USER" bash -c "/home/$SUDO_USER/.cargo/bin/cargo install tree-sitter-cli"
+        echo "$pkg is installed." && continue
+    fi
+
     if ! deb_installed "$pkg"; then
         apt install -y "$pkg"
     fi
     echo "$pkg is installed." && continue
 done
+
+# https://wiki.archlinux.org/title/SSH_keys
+ssh_agent_setup_path="$HOME/.local/ssh-agent-setup"
+if [ ! -f "$ssh_agent_setup_path" ]; then
+    cat >"$ssh_agent_setup_path" << EOF
+if ! pgrep -u "\$USER" ssh-agent > /dev/null; then
+    ssh-agent -t 24h > "\$HOME/.ssh/ssh-agent.env"
+fi
+if [[ ! -f "\$SSH_AUTH_SOCK" ]]; then
+    source "\$HOME/.ssh/ssh-agent.env" >/dev/null
+fi
+EOF
+fi
+if ! grep -qF "source $ssh_agent_setup_path" "$HOME/.bashrc" ; then
+    echo "source $ssh_agent_setup_path" >> "$HOME/.bashrc"
+fi
 
 chown -R "$SUDO_USER:$SUDO_USER" "/home/$SUDO_USER/.config"
 chown -R "$SUDO_USER:$SUDO_USER" "/home/$SUDO_USER/.cache"
